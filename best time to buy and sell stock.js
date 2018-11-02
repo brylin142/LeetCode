28d82e06ac6644ff3f99435cc1dfd30201cb177e
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-11-01 17:21:07 -0700

    Solved

---
+++ best time to buy and sell stock.js
@@ -0,0 +1,14 @@
+// /**
+//  * @param {number[]} prices
+//  * @return {number}
+//  */
+
+var maxProfit = function (prices) {
+  let min = prices[0];
+  let profit = 0;
+  for (i = 1; i < prices.length; i++) {
+    if (profit < prices[i] - min) profit = prices[i] - min
+    if (prices[i] < min) min = prices[i]
+  }
+  return profit;
+};
